64aab2bc2860ef413a535b7baa3b5456f496f9dc
Ed Burns <edward.burns@oracle.com>
2006-05-04 16:40:09 +0000

    M      samples/cardemo/src/main/webapp/devtime.js
    - Have the dynamic buttons working.  Need to fix the jsp:loadBundle
      problem.
    
    svn path=/trunk/; revision=28

--- code/samples/cardemo/src/main/webapp/devtime.js
+++ code/samples/cardemo/src/main/webapp/devtime.js
@@ -29,21 +29,123 @@
 
 function extractParams(ajaxZone, element, originalScript, outProps, 
 		       invocation) {
-    collectParamsFromInputChildren(ajaxZone, outProps);
+    collectParams(ajaxZone, outProps);
+    // Get the params from this element only
+    var name = null;
+    var value = null;
+
+    value = element.value;
+    
+    // If this is an option element...
+    if (-1 != element.nodeName.toLowerCase().indexOf("option")) {
+	// the name comes from the parent <select> element
+	while (null != (element = element.parentNode) &&
+	       -1 == element.nodeName.toLowerCase().indexOf("select"));
+    }
+    if (null != element) {
+	if (null == (name = element.name)) {
+	    name = element.id;
+	}
+    }
+    
+    outProps[name] = value;
+
     invocation.args[0].preventDefault();
     invocation.args[0].stopPropagation();
 }
 
-function inspectElement(element) {
-  var result = false
-  if (null != element) {
-    var nodeName = element.nodeName;
-    if (null != nodeName) {
-      if (-1 != nodeName.toLowerCase().indexOf("input")) {
-        result = true;
-      }
+/**
+ * If the element is <input type="button">, <input type="submit">, or
+ * <button>, take no action.
+ *
+ * If the element is any other kind of input or <option> element,
+ * collect the params from it.
+ */
+
+function collectParams(element, props) {
+    var name = null;
+    var i = 0;
+    var doCollect = isCollectParams(element);
+
+    if (doCollect) {
+	if (null == (name = element.name)) {
+	    name = element.id;
+	}
+	if (null != name) {
+	    props[name] = element.value;
+	}
     }
-  }
-  return result;
+    
+    if (element.hasChildNodes()) {
+	for (i = 0; i < element.childNodes.length; i++) {
+	    collectParams(element.childNodes[i], props);
+	}
+    }
+    return;
+}
+
+function inspectElement(element) {
+    var result = false;
+    if (null != element) {
+	var nodeName = element.nodeName;
+	if (null != nodeName) {
+	    nodeName = nodeName.toLowerCase();
+	    if (-1 != nodeName.indexOf("input")) {
+		result = true;
+	    }
+	    else if (-1 != nodeName.indexOf("option")) {
+		result = true;
+	    }
+	    else if (-1 != nodeName.indexOf("button")) {
+		result = true;
+	    }
+	}
+    }
+    return result;
+}
+
+
+/**
+ * return false if the element is <input type="submit">, <input
+ * type="button">, or <button>.  
+ *
+ * otherwise, if the element is any other kind of <input> element, or an
+ * <option> element, return true.
+ */
+
+function isCollectParams(element) {
+    var elementNodeName = element.nodeName;
+    var elementType = element.type;
+    var doCollect = false;
+
+    elementNodeName = elementNodeName.toLowerCase();
+    if (null != elementType) {
+	elementType = elementType.toLowerCase();
+    }
+
+    // If the element is an input element...
+    if (-1 != elementNodeName.indexOf("input")) {
+	if (null != elementType) {
+	    //  whose type is not "button", and not "submit" and not
+	    //  text (with collectText)...
+	    if (-1 == elementType.indexOf("submit") &&
+		-1 == elementType.indexOf("button")) {
+		// collect its params.
+		doCollect = true;
+	    }
+	}
+	else {
+	    // or who has no type attribute (impossible)
+	    doCollect = true;
+	}
+    }
+    else if (-1 != elementNodeName.indexOf("button")) {
+	doCollect = true;
+    }
+    else if (-1 != elementNodeName.indexOf("option")) {
+	doCollect = true
+    }
+
+    return doCollect;
 }
 

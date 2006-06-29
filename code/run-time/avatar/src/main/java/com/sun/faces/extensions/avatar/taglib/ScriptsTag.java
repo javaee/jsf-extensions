f6063b25eac34faae2adef746511de5df1b59b2e
Ed Burns <edward.burns@oracle.com>
2006-06-29 21:05:32 +0000

    first_site_publish
    svn path=/trunk/; revision=83

--- code/run-time/avatar/src/main/java/com/sun/faces/extensions/avatar/taglib/ScriptsTag.java
+++ code/run-time/avatar/src/main/java/com/sun/faces/extensions/avatar/taglib/ScriptsTag.java
@@ -1,6 +1,5 @@
 package com.sun.faces.extensions.avatar.taglib;
 
-import com.sun.faces.extensions.avatar.components.AjaxZone;
 import java.util.List;
 import java.util.Map;
 import javax.el.MethodExpression;

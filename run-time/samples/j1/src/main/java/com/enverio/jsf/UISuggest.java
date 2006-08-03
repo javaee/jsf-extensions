c64be3a7721aa121540f39b01446271560719c46
Ed Burns <edward.burns@oracle.com>
2006-08-03 18:30:56 +0000

    removed some unused code
    svn path=/branches/FACES_EVENT_20060726/; revision=174

--- run-time/samples/j1/src/main/java/com/enverio/jsf/UISuggest.java
+++ run-time/samples/j1/src/main/java/com/enverio/jsf/UISuggest.java
@@ -2,20 +2,14 @@ package com.enverio.jsf;
 
 import com.enverio.jsf.render.ServerSuggestRenderer;
 import java.io.IOException;
-import java.util.Map;
 import javax.el.MethodExpression;
 import javax.faces.component.html.HtmlInputText;
 import javax.faces.context.FacesContext;
-import javax.faces.event.PhaseEvent;
-import javax.faces.event.PhaseId;
-import javax.faces.event.PhaseListener;
-
-import com.sun.faces.extensions.avatar.event.EventCallback;
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.FacesEvent;
 import javax.faces.render.Renderer;
 
-public class UISuggest extends HtmlInputText implements PhaseListener {
+public class UISuggest extends HtmlInputText {
 
     public final static String COMPONENT_FAMILY = "com.enverio";
 
@@ -29,30 +23,6 @@ public class UISuggest extends HtmlInputText implements PhaseListener {
 
     public String getFamily() {
         return COMPONENT_FAMILY;
-    }
-
-    public void afterPhase(PhaseEvent event) {
-        FacesContext faces = event.getFacesContext();
-        Map<String, String> p = faces.getExternalContext()
-                .getRequestParameterMap();
-
-        String clientId = p.get(COMPONENT_TYPE);
-        if (clientId != null) {
-            try {
-                faces.getViewRoot().invokeOnComponent(faces, clientId, new EventCallback(clientId, "suggest"));
-            } finally {
-                // this event finishes the response
-                faces.responseComplete();
-            }
-        }
-    }
-
-    public void beforePhase(PhaseEvent event) {
-        // no op
-    }
-
-    public PhaseId getPhaseId() {
-        return PhaseId.APPLY_REQUEST_VALUES;
     }
 
     public MethodExpression getFrom() {

a2e4702050fb0971e3933d37c721faa4436f33a7
Ryan Lubke <ryan.lubke@oracle.com>
2008-07-10 22:46:44 +0000

    M      jsf-api/src/javax/faces/context/FacesContext.java  - add new method public boolean.isPostBack().    This method will be called by RestoreViewPhase() and as a side    effect it will cache the result of the postback determination for    use in UIComponentBase or any other component that is interested    in this information.
    M      jsf-ri/src/com/sun/faces/lifecycle/RestoreViewPhase.java
    M      jsf-api/src/javax/faces/component/UIComponentBase.java
     - leverage FacesContext.isPostback()
    
    M      jsf-ri/test/com/sun/faces/context/TestFacesContextImpl.java
     - basic unit test for the isPostback() implementation
    
    M      jsf-ri/src/com/sun/faces/context/FacesContextImpl.java
     - implementation of isPostback()
    
    M      lib/jsf-extensions-test-time.jar
     - added implementation of isPostback() to MockFacesContext
    
    svn path=/trunk/; revision=543

--- code/test-time/src/main/java/com/sun/faces/mock/MockFacesContext.java
+++ code/test-time/src/main/java/com/sun/faces/mock/MockFacesContext.java
@@ -40,6 +40,7 @@ import java.util.HashMap;
 import javax.faces.FactoryFinder;
 import javax.faces.application.Application;
 import javax.faces.application.FacesMessage;
+import javax.faces.application.ViewHandler;
 import javax.faces.application.FacesMessage.Severity;
 import javax.faces.component.UIComponent;
 import javax.faces.component.UIViewRoot;
@@ -53,9 +54,14 @@ import javax.faces.render.RenderKitFactory;
 
 import javax.el.ELContext;
 
+import com.sun.faces.renderkit.RenderKitUtils;
+
 
 // Mock Object for FacesContext
 public class MockFacesContext extends FacesContext {
+
+    private static final String POST_BACK_MARKER =
+           MockFacesContext.class.getName() + "_POST_BACK";
   
     private Severity maxSeverity;
 
@@ -223,6 +229,27 @@ public class MockFacesContext extends FacesContext {
     }
     public void setViewRoot(UIViewRoot root) {
         this.root = root;
+    }
+
+    @Override
+    public boolean isPostback() {
+
+        Boolean postback = (Boolean) this.getAttributes().get(POST_BACK_MARKER);
+        if (postback == null) {
+            RenderKit rk = this.getRenderKit();
+            if (rk != null) {
+                postback = rk.getResponseStateManager().isPostback(this);
+            } else {
+                // ViewRoot hasn't been set yet, so calculate the RK
+                ViewHandler vh = this.getApplication().getViewHandler();
+                String rkId = vh.calculateRenderKitId(this);
+                postback = RenderKitUtils.getResponseStateManager(this, rkId).isPostback(this);
+            }
+            this.getAttributes().put(POST_BACK_MARKER, postback);
+        }
+
+        return postback.booleanValue();
+
     }
 
 

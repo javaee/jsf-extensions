cf6d6f5b2df3e4d9c3136cbd3a33854dad9f36b5
tgiunipero <tgiunipero@localhost>
2009-03-05 23:10:36 +0000

    updated sql script.
    svn path=/branches/TROY_DEMO/; revision=589

--- code/AffableBean/AffableBean/setup/affablebean.sql
+++ code/AffableBean/AffableBean/setup/affablebean.sql
@@ -1,17 +1,21 @@
 --
+-- Affable Bean Sample Database Schema
+-- Version 1.0
+--
 -- author: troy giunipero
 -- date: 10 february 2009
 --
 
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
 --
--- Create database: `affablebean`
+-- Create SCHEMA: `affablebean`
 --
 
-CREATE DATABASE `affablebean` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+DROP SCHEMA IF EXISTS `affablebean`;
+CREATE SCHEMA `affablebean`;
+
 USE `affablebean`;
+
 
 -- --------------------------------------------------------
 
@@ -20,82 +24,13 @@ USE `affablebean`;
 --
 
 DROP TABLE IF EXISTS `category`;
-CREATE TABLE IF NOT EXISTS `category` (
-  `id` smallint(4) unsigned NOT NULL auto_increment,
-  `name` varchar(45) NOT NULL,
-  `description` text,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='describes the category which each product belongs to';
-
-
-
---
--- Table structure for table `creditcard`
---
-
-DROP TABLE IF EXISTS `creditcard`;
-CREATE TABLE IF NOT EXISTS `creditcard` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
-  `type` enum('visa','mastercard') NOT NULL,
-  `number` varchar(20) NOT NULL,
-  `expiry` varchar(7) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='credit card details';
-
-
-
---
--- Table structure for table `customer`
---
-
-DROP TABLE IF EXISTS `customer`;
-CREATE TABLE IF NOT EXISTS `customer` (
-  `id` varchar(45) NOT NULL,
-  `firstname` varchar(45) NOT NULL,
-  `familyname` varchar(45) NOT NULL,
-  `telephone` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `address1` varchar(45) NOT NULL,
-  `address2` varchar(45) NOT NULL,
-  `city` varchar(30) NOT NULL,
-  `postcode` varchar(10) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  `date_account_created` datetime NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='maintains customer details';
-
-
-
---
--- Table structure for table `customer_basket`
---
-
-DROP TABLE IF EXISTS `customer_basket`;
-CREATE TABLE IF NOT EXISTS `customer_basket` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
-  `quantity` varchar(4) NOT NULL default '1',
-  `date_last_login` datetime NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='maintains details for customer baskets';
-
-
-
---
--- Table structure for table `order`
---
-
-DROP TABLE IF EXISTS `order`;
-CREATE TABLE IF NOT EXISTS `order` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
-  `date` datetime default NULL,
-  `shippingmethod` enum('1','2','3') NOT NULL default '1',
-  `address1` varchar(45) default NULL,
-  `address2` varchar(45) default NULL,
-  `city` varchar(30) default NULL,
-  `postcode` varchar(10) default NULL,
-  `comments` text,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='maintains customer order and shipping details';
+CREATE TABLE `category` (
+  `category_id` TINYINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `image_path` VARCHAR(100) NOT NULL,
+  `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='contains product categories, e.g., dairy, meats, etc.';
 
 
 
@@ -104,9 +39,70 @@ CREATE TABLE IF NOT EXISTS `order` (
 --
 
 DROP TABLE IF EXISTS `product`;
-CREATE TABLE IF NOT EXISTS `product` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
-  `name` varchar(45) NOT NULL,
-  `price` varchar(4) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='provides product details';
+CREATE TABLE `product` (
+  `product_id` MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(50) NOT NULL,
+  `price` DECIMAL(5,2) NOT NULL,
+  `category_id` TINYINT UNSIGNED NOT NULL,
+  `last_update` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`product_id`),
+  KEY `idx_fk_category_id` (`category_id`),
+  CONSTRAINT `fk_product_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='contains product details';
+
+
+
+--
+-- Table structure for table `customer`
+--
+
+DROP TABLE IF EXISTS `customer`;
+CREATE TABLE `customer` (
+  `customer_id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `firstname` VARCHAR(50) NOT NULL,
+  `familyname` VARCHAR(50) NOT NULL,
+  `telephone` VARCHAR(50) NOT NULL,
+  `email` VARCHAR(50) NOT NULL,
+  `address1` VARCHAR(50) NOT NULL,
+  `address2` VARCHAR(50) NOT NULL,
+  `city` VARCHAR(30) NOT NULL,
+  `postcode` VARCHAR(10) NOT NULL,
+  `password` VARCHAR(30) NOT NULL,
+  `create_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='maintains customer details';
+
+
+
+--
+-- Table structure for table `order`
+--
+
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE `order` (
+  `order_id` MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `customer_id` SMALLINT UNSIGNED NOT NULL,
+  `amount` DECIMAL(5,2),
+  `status` VARCHAR(25),
+  `comments` TEXT DEFAULT NULL,
+  `create_date` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`order_id`),
+  KEY `idx_fk_customer_id` (`customer_id`),
+  CONSTRAINT `fk_order_customer` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`customer_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='maintains customer order details';
+
+
+
+--
+-- Table structure for table `ordered_product`
+--
+
+DROP TABLE IF EXISTS `ordered_product`;
+CREATE TABLE `ordered_product` (
+  `order_id` MEDIUMINT UNSIGNED NOT NULL,
+  `product_id` MEDIUMINT UNSIGNED NOT NULL,
+  `quantity` SMALLINT DEFAULT 1,
+  PRIMARY KEY (`order_id`, `product_id`),
+  CONSTRAINT `fk_ordered_product_order` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  CONSTRAINT `fk_ordered_product_product` FOREIGN KEY (`product_id`) REFERENCES `product` (`product_id`) ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='contains details for an individual product type in an order';

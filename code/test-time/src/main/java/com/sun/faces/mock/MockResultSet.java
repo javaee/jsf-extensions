bc9e8047407a79a1da71fa7d7a92ea5f9fdeb352
Ed Burns <edward.burns@oracle.com>
2014-03-21 13:48:13 +0000

    M       src/main/java/com/sun/faces/mock/MockResultSet.java
    - Remove @Overrides that cause compilation failure on 1.6
    
    M       pom.xml
    
    - Set version to 2.4
    
    svn path=/trunk/; revision=804

--- code/test-time/src/main/java/com/sun/faces/mock/MockResultSet.java
+++ code/test-time/src/main/java/com/sun/faces/mock/MockResultSet.java
@@ -1173,12 +1173,10 @@ public class MockResultSet implements ResultSet {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    @Override
     public <T> T getObject(int columnIndex, Class<T> type) throws SQLException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
     public <T> T getObject(String columnLabel, Class<T> type) throws SQLException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
